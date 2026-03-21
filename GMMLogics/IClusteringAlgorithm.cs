54e92630e936dcf5148953202302c3767add454d
Аллен <raea88532@gmail.com>
2026-03-21 21:33:15 +0300

    создание необходимых библиотек классов для работы с алгоритмом

---
+++ GMMLogics/IClusteringAlgorithm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMMLogics
+{
+    public class IClusteringAlgorithm
+    {
+        /// Здесь интерфейс для моей версии (м.б. нескольких версий) и АккордНет 
+    }
+}
---
+++ ImageParser/ImageLoader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageParser
+{
+    public class ImageLoader
+    {
+        /// Тут будет логика разбиения изображения на пиксели
+    }
+}
